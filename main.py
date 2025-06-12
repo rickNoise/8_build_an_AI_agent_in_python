012d93b9d8c4249dcc1a3a364bbdee156a4a26a5
rickNoise <emberix@gmail.com>
2025-06-12 10:39:46 +1000

    command line argument is now the prompt

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        raise ValueError("Prompt not provided as a command line argument.")
+    user_prompt = sys.argv[1]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=contents
+        contents=user_prompt
     )
+    
     print(f"Response: {response.text}")
     usage_metadata = response.usage_metadata
     print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
