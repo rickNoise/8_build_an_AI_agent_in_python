6ecdb76fd93c7e16da6b26adb18b4d4b7762c91e
rickNoise <emberix@gmail.com>
2025-06-12 10:27:47 +1000

    added main.py to test Gemini API key

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+)
+print(f"Response: {response.text}")
+usage_metadata = response.usage_metadata
+print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+print(f"Response tokens: {usage_metadata.candidates_token_count}")
