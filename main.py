b8575ec882fc784ef1366f878d7e0decbb249bc6
rickNoise <emberix@gmail.com>
2025-06-12 11:02:55 +1000

    thru CH1 L6 - adding --verbose flag

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
     if not args:
         print("AI Code Assistant")
         print("\nUsage: python main.py 'your prompt here'")
         print("Example: python main.py 'How do I build a calculator app?'")
+        print("Optional --verbose flag means more output information will show.")
         sys.exit(1)
+    
     user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -27,10 +33,12 @@ def main():
         contents=messages,
     )
     
-    print(f"Response: {response.text}")
-    usage_metadata = response.usage_metadata
-    print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {usage_metadata.candidates_token_count}")
+    if verbose:
+        usage_metadata = response.usage_metadata
+        print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
