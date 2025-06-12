30db74a0ac38c49242814a57b25cc30f594a09bd
rickNoise <emberix@gmail.com>
2025-06-12 10:31:23 +1000

    cleaned up main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import os
+import os, sys
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-
 from google import genai
 
-client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
-print(f"Response: {response.text}")
-usage_metadata = response.usage_metadata
-print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-print(f"Response tokens: {usage_metadata.candidates_token_count}")
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=contents
+    )
+    print(f"Response: {response.text}")
+    usage_metadata = response.usage_metadata
+    print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
